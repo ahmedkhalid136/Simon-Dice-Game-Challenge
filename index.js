7abf6238fd857965e484946ce6c4fbb9f039b7f7
ahmedkhalid136 <64730807+ahmedkhalid136@users.noreply.github.com>
2020-08-25 15:51:27 +0500

    Used Switch statement instead of if else

--- index.js
+++ index.js
@@ -5,61 +5,71 @@ var player2;
 player1=Math.floor(Math.random()*6)+1;
 player2=Math.floor(Math.random()*6)+1;
 
-// Player1 Checker
+// player1 Checker
 
-if(player1===1)
+switch(player1)
 {
-    document.querySelector("img.img1").setAttribute("src","./images/dice1.png");
-}
-else if(player1===2)
-{
-    document.querySelector("img.img1").setAttribute("src","./images/dice2.png");
-}
-else if(player1===3)
-{
-    document.querySelector("img.img1").setAttribute("src","./images/dice3.png");
-}
-else if(player1===4)
-{
-    document.querySelector("img.img1").setAttribute("src","./images/dice4.png");
-}
-else if(player1===5)
-{
-    document.querySelector("img.img1").setAttribute("src","./images/dice5.png");
-}
-else if(player1===6)
-{
-    document.querySelector("img.img1").setAttribute("src","./images/dice6.png");
-}
+    case 1:
+        document.querySelector("img.img1").setAttribute("src","./images/dice1.png");
+        break;
+    
+    case 2:
+        document.querySelector("img.img1").setAttribute("src","./images/dice2.png");
+        break;
+       
+    case 3:
+        document.querySelector("img.img1").setAttribute("src","./images/dice3.png");
+        break;
+        
+    case 4:
+        document.querySelector("img.img1").setAttribute("src","./images/dice4.png");
+        break;
 
+    case 5:
+        document.querySelector("img.img1").setAttribute("src","./images/dice5.png");
+        break;
+    
+    case 6:
+        document.querySelector("img.img1").setAttribute("src","./images/dice6.png");
+        break;
+
+    default:
+        break;
+}
 
 // Player 2 Checker
 
 
-if(player2===1)
+switch(player2)
 {
-    document.querySelector("img.img2").setAttribute("src","./images/dice1.png");
+    case 1:
+        document.querySelector("img.img2").setAttribute("src","./images/dice1.png");
+        break;
+    
+    case 2:
+        document.querySelector("img.img2").setAttribute("src","./images/dice2.png");
+        break;
+       
+    case 3:
+        document.querySelector("img.img2").setAttribute("src","./images/dice3.png");
+        break;
+        
+    case 4:
+        document.querySelector("img.img2").setAttribute("src","./images/dice4.png");
+        break;
+
+    case 5:
+        document.querySelector("img.img2").setAttribute("src","./images/dice5.png");
+        break;
+    
+    case 6:
+        document.querySelector("img.img2").setAttribute("src","./images/dice6.png");
+        break;
+
+    default:
+        break;
 }
-else if(player2===2)
-{
-    document.querySelector("img.img2").setAttribute("src","./images/dice2.png");
-}
-else if(player2===3)
-{
-    document.querySelector("img.img2").setAttribute("src","./images/dice3.png");
-}
-else if(player2===4)
-{
-    document.querySelector("img.img2").setAttribute("src","./images/dice4.png");
-}
-else if(player2===5)
-{
-    document.querySelector("img.img2").setAttribute("src","./images/dice5.png");
-}
-else if(player2===6)
-{
-    document.querySelector("img.img2").setAttribute("src","./images/dice6.png");
-}
+
 
 
 //Winner Checker
